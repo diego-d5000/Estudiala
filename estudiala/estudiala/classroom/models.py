c2ed3f5e18d77f8bed1a856795d0f1e6804369a6
Alexis Torreblanca <alexis_torreblanca@outlook.com>
2015-05-27 19:35:53 +0200

    Added DateTimeField in Classroom models

--- estudiala/estudiala/classroom/models.py
+++ estudiala/estudiala/classroom/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 class Room_type(models.Model):
 	room_typ = models.CharField(max_length=225)
 
@@ -17,7 +18,7 @@ class Chat(models.Model):
 	username = models.CharField(max_length=64)
 	role = models.CharField(max_length=15)
 	text = models.CharField(max_length=280)
-	date = models.CharField(max_length=35)
+	date = models.DateTimeField(default=timezone.now)
 	room = models.ForeignKey(Room)
 
 	def __unicode__(self):
